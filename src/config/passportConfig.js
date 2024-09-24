cc85de546a5687366cf7abf395e43f8bc677fd6e
Valentina Jimenez <jimenez.valentina2002@gmail.com>
2024-09-23 21:31:20 -0300

    cambios4

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -22,10 +22,15 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, passwor
     }
 }))
 
+const isProduction = process.env.LOGGER_ENV
+console.log(isProduction)
+
  passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "https://proyectofinalbackend-production-4db6.up.railway.app/auth/github/callback"
+    callbackURL: isProduction
+    ? "http://localhost:8080/auth/github/callback"
+    : "https://proyectofinalbackend-production-4db6.up.railway.app/auth/github/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         if (!profile.emails || !profile.emails[0].value) {
